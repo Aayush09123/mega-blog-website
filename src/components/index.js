f68b7c63f00ae3428224ed0e32ee59c4c3013bfc
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-04-02 12:23:16 +0530

    add LogoutBtn component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
-export { Header }
+export { Header, Footer, Logo, LogoutBtn }
