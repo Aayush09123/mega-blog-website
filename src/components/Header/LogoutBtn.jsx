08b69167832951cd07bb54801e0bec768c62529a
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-04-09 12:55:08 +0530

    add logout functionality

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -14,7 +14,12 @@ const LogoutBtn = () => {
     }
 
     return (
-        <button className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>LogoutBtn</button>
+        <button
+            className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+            onClick={logoutHandler}
+        >
+            LogoutBtn
+        </button>
     )
 }
 
