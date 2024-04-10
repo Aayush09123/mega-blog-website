26460e931c783129c5617b0a0d21872bd3f47a8d
Aayush09123 <110332733+Aayush09123@users.noreply.github.com>
2024-04-10 10:51:23 +0530

    render components in App file

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useDispatch } from "react-redux";
 import authService from './appwrite/auth';
 import { login, logout } from './store/authSlice';
 import './App.css'
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -21,11 +23,17 @@ function App() {
       .finally(() => setLoading(false))
   }, [dispatch]);
 
-  return (
-    <>
-      <h1>Mega Blog Website</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
